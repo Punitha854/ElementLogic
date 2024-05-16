95edcf8f556e30fcbf6a6157f9706653b794e8d4
Punitha854 <55803350+Punitha854@users.noreply.github.com>
2024-05-16 06:57:06 +0200

    adding about content
    adding about content

--- WorkdayCalendar/Program.cs
+++ WorkdayCalendar/Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WorkdayCalendar;
 using Syncfusion.Blazor;
+using Syncfusion.Blazor.Popups;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton<WorkingDayCalendar>(); // Dependency injection
 builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<SfDialogService>();
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
